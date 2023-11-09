d7fd4ffdea2a32d4e3508c13f9e4d82e534b6ad1
Cole <Cole@example.com>
2023-11-09 16:41:05 -0700

    Comments

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ for count in range(start, max(num_list)):
     if count not in num_list:
         print(f"{count} is missing")      
 # Takes number in "three_digit_numbers.txt"
-# Shows which numbers are missing as well as in order\
+# Shows which numbers are missing as well as in order
 sorted_nums = str(sorted(cols))
 sorted_numbers = open("sorted_numbers.txt", "w")
 sorted_numbers.write(sorted_nums)
+#Takes nums and sorts them in the file
 f.close()
 sorted_numbers.close()
